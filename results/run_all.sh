9c4d03afcba192009504259e67daa764050f46ab
Nathan Chong <nyc04@doc.ic.ac.uk>
2011-05-04 17:28:49 +0100

    Scripts to generate results.

--- results/run_all.sh
+++ results/run_all.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-PROGRAMS=(../serial/serial_gpu_timer ../cuda/cuda_gpu_timer ../op2/op2_gpu_timer )
-OUTPUTS=( serial.data                cuda.data              op2.data )
-DATADIR=../data
-NUMITER=1000
-
-for ((i=0; i<${#PROGRAMS[@]}; i++)); do
-  PROGRAM=${PROGRAMS[$i]}
-  OUTPUT=${OUTPUTS[$i]}
-  rm -f ${OUTPUT}
-
-  for j in {1..10}; do
-    if [ $j == 1 ]; then
-      ${PROGRAM} ${DATADIR}/${j}000.step -n ${NUMITER} -v 2> /dev/null | tee -a ${OUTPUT};
-    else
-      ${PROGRAM} ${DATADIR}/${j}000.step -n ${NUMITER}    2> /dev/null | tee -a ${OUTPUT};
-    fi
-  done
-done
+cd ../serial; ./run.sh; cd -
+cd ../cuda; PROGRAM=./cuda_gpu_timer ./run.sh; cd -
+cd ../op2; PROGRAM=./op2_gpu_timer ./run.sh; PROGRAM=./op2_gpu_timer ./part.sh; cd -
