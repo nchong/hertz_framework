e2df6b378feaf35ec9f94df3a7487532128b4323
Nathan <nathan.chong@gmail.com>
2011-07-06 15:15:54 +0100

    Update to stepfiles from icliggghts (1.2.9).

--- serial/serial.cpp
+++ serial/serial.cpp
@@ -19,6 +19,171 @@
 
 using namespace std;
 
+double dt;
+double nktv2p;
+double yeff;
+double geff;
+double betaeff;
+double coeffFrict;
+
+inline void pair_interaction(
+    int i, int j,
+  //inputs
+    double *xi, double *xj,           //position
+    double *vi, double *vj,           //velocity
+    double *omegai, double *omegaj,   //rotational velocity
+    double radi, double radj,         //radius
+    double massi, double massj,       //mass
+    int typei, int typej,             //type
+  //inouts
+    double *shear,
+    double *forcei,
+    double *forcej,
+    double *torquei,
+    double *torquej) {
+
+  // del is the vector from j to i
+  double delx = xi[0] - xj[0];
+  double dely = xi[1] - xj[1];
+  double delz = xi[2] - xj[2];
+
+  double rsq = delx*delx + dely*dely + delz*delz;
+  double radsum = radi + radj;
+  if (rsq >= radsum*radsum) {
+    //unset non-touching atoms
+    shear[0] = 0.0;
+    shear[1] = 0.0;
+    shear[2] = 0.0;
+  } else {
+    //distance between centres of atoms i and j
+    //or, magnitude of del vector
+    double r = sqrt(rsq);
+    double rinv = 1.0/r;
+    double rsqinv = 1.0/rsq;
+
+    // relative translational velocity
+    double vr1 = vi[0] - vj[0];
+    double vr2 = vi[1] - vj[1];
+    double vr3 = vi[2] - vj[2];
+
+    // normal component
+    double vnnr = vr1*delx + vr2*dely + vr3*delz;
+    double vn1 = delx*vnnr * rsqinv;
+    double vn2 = dely*vnnr * rsqinv;
+    double vn3 = delz*vnnr * rsqinv;
+
+    // tangential component
+    double vt1 = vr1 - vn1;
+    double vt2 = vr2 - vn2;
+    double vt3 = vr3 - vn3;
+
+    // relative rotational velocity
+    double deltan = radsum-r;
+    double cri = radi-0.5*deltan;
+    double crj = radj-0.5*deltan;
+    double wr1 = (cri*omegai[0] + crj*omegaj[0]) * rinv;
+    double wr2 = (cri*omegai[1] + crj*omegaj[1]) * rinv;
+    double wr3 = (cri*omegai[2] + crj*omegaj[2]) * rinv;
+
+    // normal forces = Hookian contact + normal velocity damping
+    double meff = massi*massj/(massi+massj);
+    //not-implemented: freeze_group_bit
+
+    //derive contact model parameters (inlined)
+    //Yeff, Geff, betaeff, coeffFrict are lookup tables
+    double reff = radi * radj / (radi + radj);
+    double sqrtval = sqrt(reff * deltan);
+    double Sn = 2.    * yeff * sqrtval;
+    double St = 8.    * geff * sqrtval;
+    double kn = 4./3. * yeff * sqrtval;
+    double kt = St;
+    double gamman=-2.*sqrtFiveOverSix*betaeff*sqrt(Sn*meff);
+    double gammat=-2.*sqrtFiveOverSix*betaeff*sqrt(St*meff);
+    double xmu=coeffFrict;
+    //not-implemented if (dampflag == 0) gammat = 0;
+    kn /= nktv2p;
+    kt /= nktv2p;
+
+    double damp = gamman*vnnr*rsqinv;
+	  double ccel = kn*(radsum-r)*rinv - damp;
+
+    //not-implemented cohesionflag
+
+    // relative velocities
+    double vtr1 = vt1 - (delz*wr2-dely*wr3);
+    double vtr2 = vt2 - (delx*wr3-delz*wr1);
+    double vtr3 = vt3 - (dely*wr1-delx*wr2);
+
+    // shear history effects
+    shear[0] += vtr1 * dt;
+    shear[1] += vtr2 * dt;
+    shear[2] += vtr3 * dt;
+
+    // rotate shear displacements
+    double rsht = shear[0]*delx + shear[1]*dely + shear[2]*delz;
+    rsht *= rsqinv;
+
+    shear[0] -= rsht*delx;
+    shear[1] -= rsht*dely;
+    shear[2] -= rsht*delz;
+
+    // tangential forces = shear + tangential velocity damping
+    double fs1 = - (kt*shear[0]);
+    double fs2 = - (kt*shear[1]);
+    double fs3 = - (kt*shear[2]);
+
+    // rescale frictional displacements and forces if needed
+    double fs = sqrt(fs1*fs1 + fs2*fs2 + fs3*fs3);
+    double fn = xmu * fabs(ccel*r);
+    double shrmag = sqrt(shear[0]*shear[0] +
+                         shear[1]*shear[1] +
+                         shear[2]*shear[2]);
+    if (fs > fn) {
+      if (shrmag != 0.0) {
+        fs1 *= fn/fs;
+        fs2 *= fn/fs;
+        fs3 *= fn/fs;
+        shear[0] = -fs1/kt;
+        shear[1] = -fs2/kt;
+        shear[2] = -fs3/kt;
+      } else {
+        fs1 = 0.0;
+        fs2 = 0.0;
+        fs3 = 0.0;
+      }
+    } else {
+      fs1 -= (gammat*vtr1);
+      fs2 -= (gammat*vtr2);
+      fs3 -= (gammat*vtr3);
+    }
+
+    double fx = delx*ccel + fs1;
+    double fy = dely*ccel + fs2;
+    double fz = delz*ccel + fs3;
+
+    double tor1 = rinv * (dely*fs3 - delz*fs2);
+    double tor2 = rinv * (delz*fs1 - delx*fs3);
+    double tor3 = rinv * (delx*fs2 - dely*fs1);
+
+    // this is what we've been working up to!
+    forcei[0] += fx;
+    forcei[1] += fy;
+    forcei[2] += fz;
+
+    forcej[0] -= fx;
+    forcej[1] -= fy;
+    forcej[2] -= fz;
+
+    torquei[0] -= cri*tor1;
+    torquei[1] -= cri*tor2;
+    torquei[2] -= cri*tor3;
+
+    torquej[0] -= crj*tor1;
+    torquej[1] -= crj*tor2;
+    torquej[2] -= crj*tor3;
+  }
+}
+
 void run(struct params *input, int num_iter) {
 
   //--------------------
@@ -28,6 +193,15 @@ void run(struct params *input, int num_iter) {
   per_iter.push_back(SimpleTimer("kernel"));
 
   for (int run=0; run<num_iter; run++) {
+
+    //setup constants
+    dt = input->dt;
+    nktv2p = input->nktv2p;
+    yeff = input->yeff[3];
+    geff = input->geff[3];
+    betaeff = input->betaeff[3];
+    coeffFrict = input->coeffFrict[3];
+    print_params(input);
 
     //make some internal copies
     double *force = new double[input->nnode*3];
@@ -42,157 +216,22 @@ void run(struct params *input, int num_iter) {
       shear[e] = input->shear[e];
     }
 
-    double dt = 0.00001;
-    double nktv2p = 1;
-    double yeff = 3134796.2382445144467056;
-    double geff = 556173.5261401557363570;
-    double betaeff = -0.3578571305033167;
-    double coeffFrict = 0.5;
-
     per_iter[0].start();
     for (int e=0; e<input->nedge; e++) {
       int i = input->edge[(e*2)];
       int j = input->edge[(e*2)+1];
-
-      double delx = input->x[(i*3)  ] - input->x[(j*3)  ];
-      double dely = input->x[(i*3)+1] - input->x[(j*3)+1];
-      double delz = input->x[(i*3)+2] - input->x[(j*3)+2];
-
-      double rsq = delx*delx + dely*dely + delz*delz;
-      double radsum = input->radius[i] + input->radius[j];
-      if (rsq >= radsum*radsum) {
-        //unset non-touching atoms
-        shear[(e*3)  ] = 0.0;
-        shear[(e*3)+1] = 0.0;
-        shear[(e*3)+2] = 0.0;
-      } else {
-        //distance between centres of atoms i and j
-        //or, magnitude of del vector
-        double r = sqrt(rsq);
-        double rinv = 1.0/r;
-        double rsqinv = 1.0/rsq;
-
-        // relative translational velocity
-        double vr1 = input->v[(i*3)  ] - input->v[(j*3)  ];
-        double vr2 = input->v[(i*3)+1] - input->v[(j*3)+1];
-        double vr3 = input->v[(i*3)+2] - input->v[(j*3)+2];
-
-        // normal component
-        double vnnr = vr1*delx + vr2*dely + vr3*delz;
-        double vn1 = delx*vnnr * rsqinv;
-        double vn2 = dely*vnnr * rsqinv;
-        double vn3 = delz*vnnr * rsqinv;
-
-        // tangential component
-        double vt1 = vr1 - vn1;
-        double vt2 = vr2 - vn2;
-        double vt3 = vr3 - vn3;
-
-        // relative rotational velocity
-        double wr1 = (input->radius[i]*input->omega[(i*3)  ] + 
-                      input->radius[j]*input->omega[(j*3)  ]) * rinv;
-        double wr2 = (input->radius[i]*input->omega[(i*3)+1] + 
-                      input->radius[j]*input->omega[(j*3)+1]) * rinv;
-        double wr3 = (input->radius[i]*input->omega[(i*3)+2] + 
-                      input->radius[j]*input->omega[(j*3)+2]) * rinv;
-
-        // normal forces = Hookian contact + normal velocity damping
-        double meff = input->mass[i]*input->mass[j]/(input->mass[i]+input->mass[j]);
-        //not-implemented: freeze_group_bit
-
-        double deltan = radsum-r;
-
-        //derive contact model parameters (inlined)
-        //yeff, geff, betaeff, coeffFrict are constant lookup tables
-        double reff = input->radius[i] * input->radius[j] / (input->radius[i] + input->radius[j]);
-        double sqrtval = sqrt(reff * deltan);
-        double Sn = 2.    * yeff * sqrtval;
-        double St = 8.    * geff * sqrtval;
-        double kn = 4./3. * yeff * sqrtval;
-        double kt = St;
-        double gamman=-2.*sqrtFiveOverSix*betaeff*sqrt(Sn*meff);
-        double gammat=-2.*sqrtFiveOverSix*betaeff*sqrt(St*meff);
-        double xmu=coeffFrict;
-        //not-implemented if (dampflag == 0) gammat = 0;
-        kn /= nktv2p;
-        kt /= nktv2p;
-
-        double damp = gamman*vnnr*rsqinv;
-        double ccel = kn*(radsum-r)*rinv - damp;
-
-        //not-implemented cohesionflag
-
-        // relative velocities
-        double vtr1 = vt1 - (delz*wr2-dely*wr3);
-        double vtr2 = vt2 - (delx*wr3-delz*wr1);
-        double vtr3 = vt3 - (dely*wr1-delx*wr2);
-
-        // shear history effects
-        shear[(e*3)  ] += vtr1 * dt;
-        shear[(e*3)+1] += vtr2 * dt;
-        shear[(e*3)+2] += vtr3 * dt;
-
-        // rotate shear displacements
-        double rsht = shear[(e*3)  ]*delx + 
-          shear[(e*3)+1]*dely + 
-          shear[(e*3)+2]*delz;
-        rsht *= rsqinv;
-
-        shear[(e*3)  ] -= rsht*delx;
-        shear[(e*3)+1] -= rsht*dely;
-        shear[(e*3)+2] -= rsht*delz;
-
-        // tangential forces = shear + tangential velocity damping
-        double fs1 = - (kt*shear[(e*3)  ] + gammat*vtr1);
-        double fs2 = - (kt*shear[(e*3)+1] + gammat*vtr2);
-        double fs3 = - (kt*shear[(e*3)+2] + gammat*vtr3);
-
-        // rescale frictional displacements and forces if needed
-        double fs = sqrt(fs1*fs1 + fs2*fs2 + fs3*fs3);
-        double fn = xmu * fabs(ccel*r);
-        double shrmag = 0;
-        if (fs > fn) {
-          shrmag = sqrt(
-              shear[(e*3)  ]*shear[(e*3)  ] +
-              shear[(e*3)+1]*shear[(e*3)+1] +
-              shear[(e*3)+2]*shear[(e*3)+2]);
-          if (shrmag != 0.0) {
-            shear[(e*3)  ] = (fn/fs) * (shear[(e*3)  ] + gammat*vtr1/kt) - gammat*vtr1/kt;
-            shear[(e*3)+1] = (fn/fs) * (shear[(e*3)+1] + gammat*vtr2/kt) - gammat*vtr2/kt;
-            shear[(e*3)+2] = (fn/fs) * (shear[(e*3)+2] + gammat*vtr3/kt) - gammat*vtr3/kt;
-            fs1 *= fn/fs;
-            fs2 *= fn/fs;
-            fs3 *= fn/fs;
-          } else {
-            fs1 = fs2 = fs3 = 0.0;
-          }
-        }
-
-        double fx = delx*ccel + fs1;
-        double fy = dely*ccel + fs2;
-        double fz = delz*ccel + fs3;
-
-        double tor1 = rinv * (dely*fs3 - delz*fs2);
-        double tor2 = rinv * (delz*fs1 - delx*fs3);
-        double tor3 = rinv * (delx*fs2 - dely*fs1);
-
-        // this is what we've been working up to!
-        force[(i*3)  ] += fx;
-        force[(i*3)+1] += fy;
-        force[(i*3)+2] += fz;
-
-        force[(j*3)  ] -= fx;
-        force[(j*3)+1] -= fy;
-        force[(j*3)+2] -= fz;
-
-        torque[(i*3)  ] -= input->radius[i]*tor1;
-        torque[(i*3)+1] -= input->radius[i]*tor2;
-        torque[(i*3)+2] -= input->radius[i]*tor3;
-
-        torque[(j*3)  ] -= input->radius[j]*tor1;
-        torque[(j*3)+1] -= input->radius[j]*tor2;
-        torque[(j*3)+2] -= input->radius[j]*tor3;
-      }
+      pair_interaction(
+        i, j,
+        &input->x[(i*3)],     &input->x[(j*3)],
+        &input->v[(i*3)],     &input->v[(j*3)],
+        &input->omega[(i*3)], &input->omega[(j*3)],
+         input->radius[i],     input->radius[j],
+         input->mass[i],       input->mass[j],
+         input->type[i],       input->type[j],
+        &shear[(e*3)],
+        &force[(i*3)],        &force[(j*3)],
+        &torque[(i*3)],       &torque[(j*3)]
+      );
     }
     per_iter[0].stop_and_add_to_total();
 
