4ad8f28660605789029c48c66d4e36fb4ab77926
Nathan <nathan.chong@gmail.com>
2011-07-06 19:32:12 +0100

    Migrate neigh implementation to using common pair interaction.
    Results are not checking for non-newton implementations.
    This is because for pair_interaction(xi, xj, ...) and pair_interaction(xj, xj, ...) forcei != -forcei.
    Maybe due to FP rounding?

--- neigh/neigh.cu
+++ neigh/neigh.cu
@@ -4,11 +4,12 @@
 
 #define NSLOT 96          //< maximum number of neighbors per particle
 #define AOS_LAYOUT      //< AoS or SoA layout for read-only particle data
-//#define KERNEL_PRINT    //< debug printing in kernel
 //#define MAP_BUILD_CHECK //< bounds and sanity checking in build_inverse_map
 //#define NEWTON_THIRD    //< use Newton's third law to halve computation
 //#define COMPUTE_TPA     //< thread-per-atom decomposition
 #define PINNED_MEM      //< use pinned-memory for kernel output
+//#define KERNEL_PRINT    //< debug printing in kernel
+//#define DEBUG           //< add (i,j) index information to struct
 
 #ifdef GPU_TIMER
   #include "cuda_timer.h"
@@ -23,6 +24,7 @@
 #include "framework.h"
 #include "hertz_constants.h"
 #include "inverse_map.h"
+#include "pair_interaction.h"
 #include "particle.h"
 #include <sstream>
 
@@ -31,157 +33,6 @@ using namespace std;
 // --------------------------------------------------------------------------
 // DEVICE KERNEL
 // --------------------------------------------------------------------------
-
-__device__ void pair_interaction(
-  //inputs
-    double *xi, double *xj,           //position
-    double *vi, double *vj,           //velocity
-    double *omegai, double *omegaj,   //rotational velocity
-    double radi, double radj,         //radius
-    double massi, double massj,       //mass
-    int typei, int typej,             //type
-  //inouts
-    double *shear,
-    double *torque,
-    double *force
-#ifdef NEWTON_THIRD
-    , double *torquej
-#endif
-  ) {
-
-  // del is the vector from j to i
-  double delx = xi[0] - xj[0];
-  double dely = xi[1] - xj[1];
-  double delz = xi[2] - xj[2];
-
-  double rsq = delx*delx + dely*dely + delz*delz;
-  double radsum = radi + radj;
-  if (rsq >= radsum*radsum) {
-    //unset non-touching atoms
-    shear[0] = 0.0;
-    shear[1] = 0.0;
-    shear[2] = 0.0;
-  } else {
-    //distance between centres of atoms i and j
-    //or, magnitude of del vector
-    double r = sqrt(rsq);
-    double rinv = 1.0/r;
-    double rsqinv = 1.0/rsq;
-
-    // relative translational velocity
-    double vr1 = vi[0] - vj[0];
-    double vr2 = vi[1] - vj[1];
-    double vr3 = vi[2] - vj[2];
-
-    // normal component
-    double vnnr = vr1*delx + vr2*dely + vr3*delz;
-    double vn1 = delx*vnnr * rsqinv;
-    double vn2 = dely*vnnr * rsqinv;
-    double vn3 = delz*vnnr * rsqinv;
-
-    // tangential component
-    double vt1 = vr1 - vn1;
-    double vt2 = vr2 - vn2;
-    double vt3 = vr3 - vn3;
-
-    // relative rotational velocity
-    double wr1 = (radi*omegai[0] + radj*omegaj[0]) * rinv;
-    double wr2 = (radi*omegai[1] + radj*omegaj[1]) * rinv;
-    double wr3 = (radi*omegai[2] + radj*omegaj[2]) * rinv;
-
-    // normal forces = Hookian contact + normal velocity damping
-    double meff = massi*massj/(massi+massj);
-    //not-implemented: freeze_group_bit
-
-    double deltan = radsum-r;
-
-    //derive contact model parameters (inlined)
-    //Yeff, Geff, betaeff, coeffFrict are lookup tables
-    double reff = radi * radj / (radi + radj);
-    double sqrtval = sqrt(reff * deltan);
-    double Sn = 2.    * d_yeff * sqrtval;
-    double St = 8.    * d_geff * sqrtval;
-    double kn = 4./3. * d_yeff * sqrtval;
-    double kt = St;
-    double gamman=-2.*sqrtFiveOverSix*d_betaeff*sqrt(Sn*meff);
-    double gammat=-2.*sqrtFiveOverSix*d_betaeff*sqrt(St*meff);
-    double xmu=d_coeffFrict;
-    //not-implemented if (dampflag == 0) gammat = 0;
-    kn /= d_nktv2p;
-    kt /= d_nktv2p;
-
-    double damp = gamman*vnnr*rsqinv;
-	  double ccel = kn*(radsum-r)*rinv - damp;
-
-    //not-implemented cohesionflag
-
-    // relative velocities
-    double vtr1 = vt1 - (delz*wr2-dely*wr3);
-    double vtr2 = vt2 - (delx*wr3-delz*wr1);
-    double vtr3 = vt3 - (dely*wr1-delx*wr2);
-
-    // shear history effects
-    shear[0] += vtr1 * d_dt;
-    shear[1] += vtr2 * d_dt;
-    shear[2] += vtr3 * d_dt;
-
-    // rotate shear displacements
-    double rsht = shear[0]*delx + shear[1]*dely + shear[2]*delz;
-    rsht *= rsqinv;
-
-    shear[0] -= rsht*delx;
-    shear[1] -= rsht*dely;
-    shear[2] -= rsht*delz;
-
-    // tangential forces = shear + tangential velocity damping
-    double fs1 = - (kt*shear[0] + gammat*vtr1);
-    double fs2 = - (kt*shear[1] + gammat*vtr2);
-    double fs3 = - (kt*shear[2] + gammat*vtr3);
-
-    // rescale frictional displacements and forces if needed
-    double fs = sqrt(fs1*fs1 + fs2*fs2 + fs3*fs3);
-    double fn = xmu * fabs(ccel*r);
-    double shrmag = 0;
-    if (fs > fn) {
-      shrmag = sqrt(shear[0]*shear[0] +
-                    shear[1]*shear[1] +
-                    shear[2]*shear[2]);
-      if (shrmag != 0.0) {
-        shear[0] = (fn/fs) * (shear[0] + gammat*vtr1/kt) - gammat*vtr1/kt;
-        shear[1] = (fn/fs) * (shear[1] + gammat*vtr2/kt) - gammat*vtr2/kt;
-        shear[2] = (fn/fs) * (shear[2] + gammat*vtr3/kt) - gammat*vtr3/kt;
-        fs1 *= fn/fs;
-        fs2 *= fn/fs;
-        fs3 *= fn/fs;
-      } else {
-        fs1 = fs2 = fs3 = 0.0;
-      }
-    }
-
-    double fx = delx*ccel + fs1;
-    double fy = dely*ccel + fs2;
-    double fz = delz*ccel + fs3;
-
-    double tor1 = rinv * (dely*fs3 - delz*fs2);
-    double tor2 = rinv * (delz*fs1 - delx*fs3);
-    double tor3 = rinv * (delx*fs2 - dely*fs1);
-
-    // this is what we've been working up to!
-    force[0] += fx;
-    force[1] += fy;
-    force[2] += fz;
-
-    torque[0] -= radi*tor1;
-    torque[1] -= radi*tor2;
-    torque[2] -= radi*tor3;
-
-#ifdef NEWTON_THIRD
-    torquej[0] -= radj*tor1;
-    torquej[1] -= radj*tor2;
-    torquej[2] -= radj*tor3;
-#endif
-  }
-}
 
 __global__ void compute_kernel_tpa(
   int nparticles,
@@ -215,8 +66,8 @@ __global__ void compute_kernel_tpa(
     double massi; double massj;
     int    typei; int typej;
     double shearij[3] = {0,0,0};
-    double force_deltai[3] = {0,0,0};
-    double torque_deltai[3] = {0,0,0};
+    double force_sum[3] = {0,0,0};
+    double torque_sum[3] = {0,0,0};
 
 #ifdef AOS_LAYOUT
     xi[0]     = particle_aos[idx].x[0];
@@ -282,16 +133,24 @@ __global__ void compute_kernel_tpa(
       shearij[1] = shear[neigh_idx].y;
       shearij[2] = shear[neigh_idx].z;
 
+      double force_deltai[3] = {0,0,0};
+      double torque_deltai[3] = {0,0,0};
+
 #ifdef NEWTON_THIRD
       double torque_deltaj[3] = {0,0,0};
       pair_interaction(
+#ifdef DEBUG
+        particle_aos[idx].idx, neigh[neigh_idx].idx,
+#endif
         xi, xj,
         vi, vj,
         omegai, omegaj,
         radiusi, radiusj,
         massi, massj,
         typei, typej,
-        shearij, torque_deltai, force_deltai, torque_deltaj);
+        shearij,
+        force_deltai, /* forcej is */ NULL,
+        torque_deltai, torque_deltaj);
 
       fdelta[neigh_idx].x = force_deltai[0];
       fdelta[neigh_idx].y = force_deltai[1];
@@ -302,26 +161,46 @@ __global__ void compute_kernel_tpa(
       tdeltaj[neigh_idx].z = torque_deltaj[2];
 #else
       pair_interaction(
+#ifdef DEBUG
+        particle_aos[idx].idx, neigh[neigh_idx].idx,
+#endif
         xi, xj,
         vi, vj,
         omegai, omegaj,
         radiusi, radiusj,
         massi, massj,
         typei, typej,
-        shearij, torque_deltai, force_deltai);
+        shearij,
+        force_deltai,  /* forcej is */ NULL,
+        torque_deltai, /* torquej is */ NULL);
 #endif
 
       shear[neigh_idx].x = shearij[0];
       shear[neigh_idx].y = shearij[1];
       shear[neigh_idx].z = shearij[2];
-    }
-    force[(idx*3)  ] += force_deltai[0];
-    force[(idx*3)+1] += force_deltai[1];
-    force[(idx*3)+2] += force_deltai[2];
 
-    torque[(idx*3)  ] += torque_deltai[0];
-    torque[(idx*3)+1] += torque_deltai[1];
-    torque[(idx*3)+2] += torque_deltai[2];
+      force_sum[0] += force_deltai[0];
+      force_sum[1] += force_deltai[1];
+      force_sum[2] += force_deltai[2];
+
+      torque_sum[0] += torque_deltai[0];
+      torque_sum[1] += torque_deltai[1];
+      torque_sum[2] += torque_deltai[2];
+
+      if (idx==10) {
+        cuPrintf("%.16f, %.16f, %.16f\n", force_deltai[0], force_deltai[1], force_deltai[2]);
+      } else if (neigh_idx==10) {
+        cuPrintf("%.16f, %.16f, %.16f\n", -force_deltai[0], -force_deltai[1], -force_deltai[2]);
+      }
+
+    }
+    force[(idx*3)  ] += force_sum[0];
+    force[(idx*3)+1] += force_sum[1];
+    force[(idx*3)+2] += force_sum[2];
+
+    torque[(idx*3)  ] += torque_sum[0];
+    torque[(idx*3)+1] += torque_sum[1];
+    torque[(idx*3)+2] += torque_sum[2];
   }
 }
 
@@ -432,13 +311,18 @@ __global__ void compute_kernel_bpa(
 #ifdef NEWTON_THIRD
     double torque_deltaj[3] = {0,0,0};
     pair_interaction(
+#ifdef DEBUG
+      particle_aos[idx].idx, neigh[neigh_idx].idx,
+#endif
       xi, xj,
       vi, vj,
       omegai, omegaj,
       radiusi, radiusj,
       massi, massj,
       typei, typej,
-      shearij, torque_deltai, force_deltai, torque_deltaj);
+      shearij,
+      force_deltai, /* forcej is */ NULL,
+      torque_deltai, torque_deltaj);
 
     fdelta[neigh_idx].x = force_deltai[0];
     fdelta[neigh_idx].y = force_deltai[1];
@@ -449,13 +333,18 @@ __global__ void compute_kernel_bpa(
     tdeltaj[neigh_idx].z = torque_deltaj[2];
 #else
     pair_interaction(
+#ifdef DEBUG
+      particle_aos[idx].idx, neigh[neigh_idx].idx,
+#endif
       xi, xj,
       vi, vj,
       omegai, omegaj,
       radiusi, radiusj,
       massi, massj,
       typei, typej,
-      shearij, torque_deltai, force_deltai);
+      shearij,
+      force_deltai,  /* forcej is */ NULL,
+      torque_deltai, /* torquej is */ NULL);
 #endif
 
     shear[neigh_idx].x = shearij[0];
@@ -801,7 +690,7 @@ void run(struct params *input, int num_iter) {
   //--------------------
   one_time.push_back(SimpleTimer("hertz_constants"));
   one_time.back().start();
-  setup_hertz_constants();
+  setup_hertz_constants(input);
   one_time.back().stop_and_add_to_total();
 
   one_time.push_back(SimpleTimer("build_particle_list"));
